4babeab215bcb2552c828e16bfad2216f6818e79
Nic_Logan <nicholaslogan255@gmail.com>
2018-06-14 13:13:02 -0500

    Logic functional

--- assets/javascript/ggJavascript.js
+++ assets/javascript/ggJavascript.js
@@ -4,17 +4,23 @@ window.onload = function () {
     var wins = 0;
     var losses = 0;
     var guessesLeft = 9;
-    var letters = "abcdefghijklmnopqrstuvwxyz"
-    var guesses = " ";
-    var letterGuessed = 'a';
+    var letters = "abcdefghijklmnopqrstuvwxyz";
+    var userGuesses = " ";
+    var letterGuessed = " ";
 
     // show variables
-    updateHTML();
+    document.getElementById("wins").innerHTML = String(wins);
+    document.getElementById("losses").innerHTML = String(losses);
+    document.getElementById("guessesRemaining").innerHTML = String(guessesLeft);
+    document.getElementById("guessesSoFar").innerHTML = String(userGuesses);
+    
+    // pick random letter
+    letterGuessed = pickRandLetter();
+    console.log("My Guess: "+ letterGuessed);
 
-
+    
     // this function will run every time we press a key
     document.onkeyup = function (event) {
-
 
         // store the key that was just pressed
         var keyPressed = event.key;
@@ -22,39 +28,82 @@ window.onload = function () {
         // check if key pressed was a letter
         if (keyPressed.length === 1 && letters.includes(keyPressed)) {
 
+            console.log("-- Key is letter");
 
             // if guessed the correct letter
             if (keyPressed == letterGuessed) {
-                wins++;
 
-                // reset game
-                var guessesLeft = 9;
-                pickRandLetter();
+                console.log("---- Correct");
+
+                wins++; // you win!
+               
+               
+                // reset guesses
+                guessesLeft = 9;
+
+                // clear old guesses
+                userGuesses = "";
+
+                // pick new letter to guess
+                letterGuessed = pickRandLetter();
+                console.log("My Guess: "+ letterGuessed);
+
             }
             else {
 
-                guessesLeft--;
-                guesses += keyPressed;
-                guesses += "";
+                console.log("---- Wrong");
+
+
+                // check if users already picked the letter
+                if(keyPressed){
+
+                }
+               
+                // recude guesses remaining
+                guessesLeft -= 1;
+                
+                 // show incorect guesses
+                 userGuesses += keyPressed;
+                 userGuesses += " ";
+
+                 // if out of guesses
+                if(guessesLeft < 1){
+
+                    losses++; // you lose!
+
+                    // reset guesses
+                    guessesLeft = 9;
+
+                    // clear old guesses
+                    userGuesses = "";
+
+                    // new guess
+                    letterGuessed = pickRandLetter();
+                    console.log("My Guess: "+ letterGuessed);
+
+                }
+               
 
             }
-            // update html
-            updateHTML();
+            
         }
 
-
+        // show variables
+        document.getElementById("wins").innerHTML = String(wins);
+        document.getElementById("losses").innerHTML = String(losses);
+        document.getElementById("guessesRemaining").innerHTML = String(guessesLeft);
+        document.getElementById("guessesSoFar").innerHTML = String(userGuesses);
     }
 }
 
-// makes the html show the values of our js file
-function updateHTML() {
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("guessesLeft").innerHTML = guessesLeft;
-    document.getElementById("guessesSoFar").innerHTML = guesses;
-}
 
-// TODO: make it pick random letter
 function pickRandLetter() {
-    return 'f';
+    var aphaChars = "abcdefghijklmnopqrstuvwxyz"
+    
+    var randind = Math.floor(Math.random()*26);
+
+    var output = aphaChars[randind];
+   
+    return output;
+
 }
